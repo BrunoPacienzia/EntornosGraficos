8a6c9429bbef6ac6ba6ec7274cf8fbedbdfb613b
brunopac <132169474+BrunoPacienzia@users.noreply.github.com>
2026-05-07 20:22:38 -0300

    practica php

---
+++ Practica4/Segunda parte/ej4/script.php
@@ -0,0 +1,8 @@
+<?php
+include("main.php");
+
+comprobar_nombre_usuario("BRUNO21");
+comprobar_nombre_usuario("bruno_21");
+comprobar_nombre_usuario("bruno-21");
+comprobar_nombre_usuario("#bruno21!");
+?>
